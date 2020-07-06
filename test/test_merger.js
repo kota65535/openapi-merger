609255f92f2584a9743e3a394491fee84481d808
Tomohiko Ozawa <tomohiko.ozawa@sony.com>
2020-07-07 01:26:31 +0900

    tests

--- test/test_merger.js
+++ test/test_merger.js
@@ -1,48 +1,49 @@
-'use strict'
+"use strict";
 
-const fs = require('fs-extra')
-const path = require('path')
-const glob = require('glob')
-const merger = require('../src/merger')
-const assert = require('chai').assert
+const fs = require("fs-extra");
+const path = require("path");
+const glob = require("glob");
+const merger = require("../src/merger");
+const assert = require("chai").assert;
 
 const runMerger = async (name) => {
-    const params = {
-        input: path.join('resources', name, 'openapi.yaml'),
-        output: path.join('resources', name, 'out.yaml'),
-        debug: true
+  const params = {
+    input: path.join("resources", name, "openapi.yaml"),
+    output: path.join("resources", name, "out.yaml"),
+    debug: true,
+  };
+  await merger(params);
+  assert.equal(
+    "" + fs.readFileSync(params.output),
+    "" + fs.readFileSync(path.join("resources", name, "expected.yaml"))
+  );
+};
+
+describe("merger", () => {
+  before(async () => {
+    let promises = [];
+    for (const f of glob.sync("**/out.yaml")) {
+      promises.push(fs.remove(f));
     }
-    await merger(params)
-    assert.equal(
-        '' + fs.readFileSync(params.output),
-        '' + fs.readFileSync(path.join('resources', name, 'expected.yaml'))
-    )
-}
+    await Promise.all(promises);
+  });
 
+  it("basic test", async () => {
+    await runMerger("petstore");
+  });
 
-describe('merger', () => {
-    before(async () => {
-        let promises = []
-        for (const f of glob.sync('**/out.yaml')) {
-            promises.push(fs.remove(f))
-        }
-        await Promise.all(promises)
-    })
+  it("with paths dir", async () => {
+    await runMerger("petstore_2");
+  });
 
-    it('basic test', async () => {
-        await runMerger('petstore')
-    });
+  it("with discriminator", async () => {
+    await runMerger("petstore_3");
+  });
 
-    it('with paths dir', async () => {
-        await runMerger('petstore_2')
-    });
-
-    it('with discriminator', async () => {
-        await runMerger('petstore_3')
-    });
-
-    it('schema name conflict', async () => {
-        await runMerger('petstore_4')
-    });
+  it("schema name conflict", async () => {
+    await runMerger("petstore_4");
+  });
+  it("http ref", async () => {
+    await runMerger("petstore_5");
+  });
 });
-
